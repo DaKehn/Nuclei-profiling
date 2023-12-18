7344ddc38a15ecc606086c56db3ed3ad6176185f
DaKehn <kehn3700@gmail.com>
2023-12-18 11:22:53 +0100

    nucelon update

--- nucleons.py
+++ nucleons.py
@@ -25,7 +25,15 @@ class Nucleon:
         }
 
     def pdf_gaussian(self):
-        # Generate 3D grid for the 'volume' grid
+        """
+        Generate 3D grid for the 'volume' grid and return 3D arrays.
+
+        Returns:
+            x (ndarray): 3D array representing the x-coordinates of the grid.
+            y (ndarray): 3D array representing the y-coordinates of the grid.
+            z (ndarray): 3D array representing the z-coordinates of the grid.
+            gaussian (ndarray): 3D array representing the Gaussian values of the grid.
+        """
         x = np.linspace(self.center_position["x"] - 2*self.nucleon_radius, self.center_position["x"] + 2*self.nucleon_radius, self.space_resolution)
         y = np.linspace(self.center_position["y"] - 2*self.nucleon_radius, self.center_position["y"] + 2*self.nucleon_radius, self.space_resolution)
         z = np.linspace(self.center_position["z"] - 2*self.nucleon_radius, self.center_position["z"] + 2*self.nucleon_radius, self.space_resolution)
